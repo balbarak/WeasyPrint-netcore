ecdf0b57098b2e44ff840dbf182c52d8fe93ef95
balbarak <balbarak@hotmail.com>
2020-07-19 18:26:57 +0300

    add latest version !

--- src/Balbarak.WeasyPrint/Resources/FileResx.Designer.cs
+++ src/Balbarak.WeasyPrint/Resources/FileResx.Designer.cs
@@ -19,7 +19,7 @@ namespace Balbarak.WeasyPrint.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class FileResx {
@@ -66,6 +66,16 @@ namespace Balbarak.WeasyPrint.Resources {
         internal static byte[] libCompress {
             get {
                 object obj = ResourceManager.GetObject("libCompress", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] win64_v51 {
+            get {
+                object obj = ResourceManager.GetObject("win64_v51", resourceCulture);
                 return ((byte[])(obj));
             }
         }
