5981b9adb428fe6cacebb32380013a2f94944041
Cleverson Nascimento <cleverson.nascimento@iatec.com>
2020-08-17 16:35:40 -0300

    Extracted IWeasyPrintClient interface to allow unit testing of application components.

---
+++ src/Balbarak.WeasyPrint/IWeasyPrintClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Balbarak.WeasyPrint
+{
+    public interface IWeasyPrintClient : IDisposable
+    {
+        event WeasyPrintClient.WeasyPrintEventHandler OnDataOutput;
+        event WeasyPrintClient.WeasyPrintEventHandler OnDataError;
+        byte[] GeneratePdf(string htmlText);
+        Task<byte[]> GeneratePdfAsync(string htmlText);
+        void GeneratePdf(string inputPathFile, string outputPathFile);
+        Task GeneratePdfAsync(string inputPathFile, string outputPathFile);
+        byte[] GeneratePdfFromUrl(string url);
+        Task<byte[]> GeneratePdfFromUrlAsync(string url);
+    }
+}
